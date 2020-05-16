fc6be6dd42fe104a2ce7e35acadbba82f9a28318
John <john@gmail.com>
2020-05-16 21:21:52 +0530

    First commit after project addition

---
+++ MSNFullAutomation/src/main/java/com/msn/automation/MSNFullAutomation/App.java
@@ -0,0 +1,13 @@
+package com.msn.automation.MSNFullAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
